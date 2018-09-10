089f16c163b9437b463a05a09727d4a08df46719
Salma Sadek <sososaluma@gmai.com>
2018-09-10 08:47:17 +0200

    add connect and disconnect

--- server/server.js
+++ server/server.js
@@ -3,12 +3,23 @@ const path = require('path') ;
 const publicPath = path.join(__dirname ,'../public') ;
 console.log(publicPath) ; */
 const path = require('path') ;
+const http = require('http') ;
 const publicPath = path.join(__dirname ,'../public') ;
 const express = require('express') ;
+const socketIO =require('socket.io') ;
 const port = process.env.PORT|| 3000 ;
 var app = express() ;
+var server =http.createServer(app) ;
+var io =socketIO(server) ;
 // to configure te static middleware
 app.use(express.static(publicPath)) ;
-app.listen(port ,()=>{
+io.on('connection' ,(socket)=>{
+console.log('new user connected') ;
+socket.on('disconnect' ,()=>{
+    console.log('Disconnected client') ;
+}) ;
+}) ;
+
+server.listen(port ,()=>{
     console.log("Server on port 3000") ;
 }) ;
